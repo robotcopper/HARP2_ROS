9fa2b885c8fd63336968a6b4d4583a933696e244
Your Name <95591405+robotcopper@users.noreply.github.com>
2024-04-07 22:16:20 +0200

    Add topic_base_hardware_interface for using micro-ros with ros2_control on real robots

--- robot_controller/launch/controller.launch.py
+++ robot_controller/launch/controller.launch.py
@@ -4,10 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, TimerAction
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, EnvironmentVariable
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, EnvironmentVariable, NotSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 import xacro
 
@@ -18,16 +19,16 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     gui = LaunchConfiguration('gui')
     world = LaunchConfiguration('world')
-    use_robot_state_pub = 'True'
-    use_joint_state_pub = 'False'
-    use_joint_state_pub_gui = 'False'
     use_rviz = LaunchConfiguration('use_rviz')
     robot_name = LaunchConfiguration('robot_name')
     pose = {'x': LaunchConfiguration('x_pose', default='0.0'),
             'y': LaunchConfiguration('y_pose', default='0.0'),
             'z': LaunchConfiguration('z_pose', default='0.04'),}
-    use_ros2_control = LaunchConfiguration('use_ros2_control')
+    use_TopicBasedSystem_hardware_interface = LaunchConfiguration('use_TopicBasedSystem_hardware_interface')
     rviz_config_file = LaunchConfiguration('rviz_config_file')
+    use_robot_state_pub = 'True'
+    # use_joint_state_pub = 'False'
+    use_joint_state_pub_gui = 'False'
 
     # Specify directory and path to file within package
     robot_description_pkg_dir = get_package_share_directory('robot_description')
@@ -36,11 +37,17 @@ def generate_launch_description():
     urdf_file_subpath = 'urdf/robot.urdf.xacro'
     robot_sim_launch_file_subpath = 'launch/robot_sim.launch.py'
     wordl_file_subpath = 'world/Table2024.world'
-    
+    if LaunchConfiguration('use_TopicBasedSystem_hardware_interface'):
+        robot_controllers = PathJoinSubstitution([FindPackageShare("robot_controller"),"params","omnidirectional_controller.yaml"])
+    else:
+        robot_controllers = PathJoinSubstitution([FindPackageShare("robot_controller"),"params","omnidirectional_controller_sim.yaml"])
+        
     # Use xacro to process the file
     xacro_file = os.path.join(robot_description_pkg_dir, urdf_file_subpath)
-    robot_description_raw = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control]) # Use of Command to replace robot_description assignment with a call to xacro process rather than Python module
+    robot_description_raw = Command(['xacro ', xacro_file, ' use_TopicBasedSystem_hardware_interface:=', use_TopicBasedSystem_hardware_interface]) # Use of Command to replace robot_description assignment with a call to xacro process rather than Python module
 
+    # reqest contents fo robot_description parameter from robot_state_publisher node
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     return LaunchDescription([
 
@@ -61,9 +68,9 @@ def generate_launch_description():
             description='Use simulation (Gazebo) clock if true'
         ),
         DeclareLaunchArgument(
-            'use_ros2_control',
-            default_value='True',
-            description='Use ros2_control if true and use Gazebo_control if false'
+            'use_TopicBasedSystem_hardware_interface',
+            default_value='False',
+            description='Use TopicBasedSystem_hardware_interface if true and use GazeboSystem_hardware_interface if false'
         ),
         DeclareLaunchArgument(
             'world',
@@ -98,26 +105,50 @@ def generate_launch_description():
                               'robot_name': robot_name,
                               'robot_description': robot_description_raw,
                               'use_robot_state_pub': use_robot_state_pub,
-                              'use_joint_state_pub': use_joint_state_pub,
+                              'use_joint_state_pub': use_TopicBasedSystem_hardware_interface,
                               'use_joint_state_pub_gui': use_joint_state_pub_gui,
                               'use_rviz': use_rviz,
                               'rviz_config_file': rviz_config_file,
                               'x_pose': pose['x'],
                               'x_pose': pose['y'],
                               'z_pose': pose['z'],
+                              'use_TopicBasedSystem_hardware_interface': use_TopicBasedSystem_hardware_interface,
+                              'use_gazebo': NotSubstitution(use_TopicBasedSystem_hardware_interface),
                              }.items()
         ),
 
+
         Node(
             package="controller_manager",
-            executable="spawner",
-            arguments=["joint_state_broadcaster"],
+            executable="ros2_control_node",
+            condition=IfCondition(use_TopicBasedSystem_hardware_interface), # Use ros2_control_node when using TopicBasedSystem because Gazebo plugin not running controller manager for us anymore
+            parameters=[{"use_sim_time": False},
+                        robot_controllers,
+            ],
+            output="both",
+            namespace=namespace,
+            remappings=[("~/robot_description", "robot_description"),]
         ),
 
         Node(
             package="controller_manager",
             executable="spawner",
-            arguments=["omnidirectional_controller"],
+            condition=UnlessCondition(use_TopicBasedSystem_hardware_interface), # Use ros2_control_node when using TopicBasedSystem because Gazebo plugin not running controller manager for us anymore
+            arguments=["joint_state_broadcaster",
+                       "-c", "/controller_manager",
+                       "-t", "joint_state_broadcaster/JointStateBroadcaster",
+                      ],
+            namespace=namespace,
+        ),
+
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["omnidirectional_controller",
+                       "-c", "/controller_manager",
+                       "-t", "omnidirectional_controllers/OmnidirectionalController",
+                      ],
+            namespace=namespace,
         ),
 
     ])
