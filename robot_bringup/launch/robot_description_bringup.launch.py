c157a5358c79101b493fdab9bc837de529d465ff
Floris Jousselin <floris.jousselin@example.com>
2024-04-09 22:05:27 +0200

    Add robot_bringup

--- robot_bringup/launch/robot_description_bringup.launch.py
+++ robot_bringup/launch/robot_description_bringup.launch.py
@@ -22,11 +22,17 @@ def generate_launch_description():
     use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
     use_joint_state_pub = LaunchConfiguration('use_joint_state_pub')
     use_joint_state_pub_gui = LaunchConfiguration('use_joint_state_pub_gui')
+    use_rviz = LaunchConfiguration('use_rviz')
+    rviz_config_file = LaunchConfiguration('rviz_config_file')
     robot_description = LaunchConfiguration('robot_description')
 
     # Specify directory and path to file within package
     robot_description_pkg_dir = get_package_share_directory('robot_description')
     urdf_file_subpath = 'urdf/robot.urdf.xacro'
+    rviz_launch_file_subpath = 'launch/rviz.launch.py'
+    rviz_config_file_subpath = 'rviz/robot_description_rviz.rviz'
+    robot_description_launch_file_subpath = 'launch/robot_description.launch.py'
+
 
     # Use xacro to process the file
     xacro_file = os.path.join(robot_description_pkg_dir, urdf_file_subpath)
@@ -66,37 +72,40 @@ def generate_launch_description():
             default_value='False',
             description='Whether to start the joint state publisher GUI'
         ),
+        DeclareLaunchArgument(
+            'use_rviz',
+            default_value='True',
+            description='Whether to start RVIZ'
+        ),
+        DeclareLaunchArgument(
+            'rviz_config_file',
+            default_value= PathJoinSubstitution([robot_description_pkg_dir, rviz_config_file_subpath ]),
+            description='Full path to the RVIZ config file to use'
+        ),
 
 
-		Node(
-        condition=IfCondition(use_robot_state_pub),
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        namespace=namespace,
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time,
-                     'robot_description':robot_description,
-                    }],
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(robot_description_pkg_dir, robot_description_launch_file_subpath)
+            ),
+            launch_arguments={'namespace': namespace,
+                              'use_sim_time': use_sim_time,
+                              'use_robot_state_pub': use_robot_state_pub,
+                              'use_joint_state_pub': use_joint_state_pub,
+                              'use_joint_state_pub_gui': use_joint_state_pub_gui,
+                              'robot_description': robot_description,
+                             }.items()
         ),
-	    
-		Node(
-        condition=IfCondition(use_joint_state_pub),
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        namespace=namespace,
-        parameters=[{'use_sim_time': use_sim_time,
-                     'robot_description': robot_description,
-                    }],
-    	),
-        
-		Node(
-        condition=IfCondition(use_joint_state_pub_gui),
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        namespace=namespace,
-        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(robot_description_pkg_dir, rviz_launch_file_subpath)
+            ),
+            condition=IfCondition(use_rviz),
+            launch_arguments={'namespace': namespace,
+                            'rviz_config': rviz_config_file,
+                            }.items()
+        )
 
     ])
