f4f57672b9da92cf30e3bb1967c44b88995d32bc
robotcopper <95591405+robotcopper@users.noreply.github.com>
2024-11-15 23:50:56 +0100

    Refactor keyboard_control launch file to launch only the necessary nodes

--- robot_bringup/launch/keyboard_control.launch.py
+++ robot_bringup/launch/keyboard_control.launch.py
@@ -19,9 +19,17 @@ def generate_launch_description():
 
     # Specify directory and path to file within package
     robot_bringup_pkg_dir = get_package_share_directory('robot_bringup')
+    robot_description_pkg_dir = get_package_share_directory('robot_description')
+    robot_controller_pkg_dir = get_package_share_directory('robot_controller')
     nav2_bringup_launch_file_subpath = 'launch/nav2_bringup.launch.py'
+    controller_launch_file_subpath = 'launch/controller.launch.py'
+    robot_description_launch_file_subpath = 'launch/robot_description.launch.py'
+    urdf_file_subpath = 'urdf/robot.urdf.xacro'
 
 
+    # Use xacro to process the file
+    xacro_file = os.path.join(robot_description_pkg_dir, urdf_file_subpath)
+    robot_description_raw = Command(['xacro ', xacro_file, ' use_controller:=', 'True', ' use_TopicBasedSystem_hardware_interface:=', 'True']) # Use of Command to replace robot_description assignment with a call to xacro process rather than Python module
 
     return LaunchDescription([
 
@@ -41,29 +49,32 @@ def generate_launch_description():
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(robot_bringup_pkg_dir, nav2_bringup_launch_file_subpath)
+                os.path.join(robot_description_pkg_dir, robot_description_launch_file_subpath)
+            ),
+            launch_arguments={'namespace': namespace,
+                              'use_sim_time': 'False',
+                              'use_robot_state_pub':'True',
+                              'use_joint_state_pub': 'True',
+                              'use_joint_state_pub_gui': 'False',
+                              'robot_description': robot_description_raw
+                             }.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(robot_controller_pkg_dir, controller_launch_file_subpath)
             ),
             launch_arguments={'namespace': namespace,
                               'use_TopicBasedSystem_hardware_interface': 'True',
-                              'use_gazebo_gui': 'True',
-                              'use_rviz': 'False',
                              }.items()
         ),
 
         ExecuteProcess(
             cmd=[
                 'gnome-terminal', '--', 'ros2', 'run', 'teleop_twist_keyboard', 'teleop_twist_keyboard',
-                '--ros-args', '--remap', '/cmd_vel:=/omnidirectional_controller/cmd_vel_unstamped'
+                '--ros-args', '--remap', '/cmd_vel:=/keyboard_cmd_vel'
             ],
-            output='screen'
+            output='screen',
         ),
-
-        # ExecuteProcess(
-        #     cmd=[
-        #         'terminator', '-e', 
-        #         'bash -c "source /opt/ros/humble/setup.bash && ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap /cmd_vel:=/omnidirectional_controller/cmd_vel_unstamped; exec bash"'
-        #     ],
-        #     output='screen'
-        # ),
 
     ])
