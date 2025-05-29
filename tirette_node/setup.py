c2ab3fdf03ee6cbc89373aac8269f75352ef2641
robotcopper <95591405+robotcopper@users.noreply.github.com>
2025-05-29 09:21:27 +0200

    feat: add serie1 node (homologation + 100 sec)

--- tirette_node/setup.py
+++ tirette_node/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'tirette_node'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),  # Ajout launch/
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'tirette_node = tirette_node.tirette_node:main'
+            'tirette_node = tirette_node.tirette_node:main',
+            'serie1 = tirette_node.serie1:main',
         ],
     },
 )
