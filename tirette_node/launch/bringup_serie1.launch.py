080ea76916a62108bba38e08bfc4ad6b1f86e5fd
robotcopper <95591405+robotcopper@users.noreply.github.com>
2025-05-31 09:00:26 +0200

    feat: add workng strategy for serie 1

--- tirette_node/launch/bringup_serie1.launch.py
+++ tirette_node/launch/bringup_serie1.launch.py
@@ -1,24 +1,18 @@
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
 from launch_ros.actions import Node
 import os
-from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
-    # Chemins vers les fichiers nécessaires
-    lidar_params_file = "/home/harp_2/ros2_ws/src/HARP2/robot_bringup/params/TminiPro.yaml"
-    
-    # Suppose que robot_bringup et ydlidar_ros2_driver sont bien trouvés via ament
-    # Sinon, utilise directement les chemins absolus pour les fichiers .launch.py
+    # Chemin vers le fichier de paramètres du LIDAR
+    # lidar_params_file = "/home/harp_2/ros2_ws/src/HARP2_ROS/robot_bringup/params/TminiPro.yaml"
+    lidar_params_file = "/home/harp_2/ros2_ws/src/HARP2_ROS/robot_bringup/params/X4.yaml"
 
     return LaunchDescription([
 
-        # Launch MicroROS
+        # Micro-ROS Agent
         ExecuteProcess(
             cmd=[
                 'ros2', 'run', 'micro_ros_agent', 'micro_ros_agent', 'serial', '--dev', '/dev/pico_mobile_base'
@@ -26,39 +20,62 @@ def generate_launch_description():
             output='screen'
         ),
 
-        # Launch YDLIDAR
+        # Lancement du driver YDLIDAR avec les bons paramètres
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
+            PythonLaunchDescriptionSource(
                 os.path.join(
                     get_package_share_directory('ydlidar_ros2_driver'),
                     'launch',
                     'ydlidar_launch.py'
                 )
-            ]),
+            ),
             launch_arguments={'params_file': lidar_params_file}.items()
         ),
 
-        # Launch robot_bringup
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                os.path.join(
-                    get_package_share_directory('robot_bringup'),
-                    'launch',
-                    'robot_controller_bringup.launch.py'
-                )
-            ]),
-            launch_arguments={
-                'use_rviz': 'False',
-                'use_gazebo': 'False',
-                'use_TopicBasedSystem_hardware_interface': 'True'
-            }.items()
+        # Lancement différé du controller
+        TimerAction(
+            period=3.0,
+            actions=[
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(
+                            get_package_share_directory('robot_bringup'),
+                            'launch',
+                            'robot_controller_bringup.launch.py'
+                        )
+                    ),
+                    launch_arguments={
+                        'use_rviz': 'False',
+                        'use_gazebo': 'False',
+                        'use_TopicBasedSystem_hardware_interface': 'True'
+                    }.items()
+                ),
+            ]
         ),
 
-        # Run tirette_node
-        Node(
-            package='tirette_node',
-            executable='serie1',
-            name='serie1',
-            output='screen'
-        )
+        # TimerAction(
+        #     period=12.0,
+        #     actions=[
+        #         Node(
+        #             package='ros2_lx16a_driver',
+        #             executable='lx16a_node.py',
+        #             name='lx16a_node',
+        #             output='screen'
+        #         ),
+        #     ]
+        # ),
+
+        # Lancement différé du nœud tirette
+        TimerAction(
+            period=8.0,
+            actions=[
+                Node(
+                    package='tirette_node',
+                    executable='serie1',
+                    name='tirette_node',
+                    output='screen'
+                )
+            ]
+        ),
+        
     ])
